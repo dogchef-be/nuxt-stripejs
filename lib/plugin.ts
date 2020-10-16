fcf91594730131eee504e1e0fd3fab2b3813136c
João Cruz <joao.fs.cruz97@gmail.com>
2020-10-16 10:29:37 +0000

    feat: add locale

--- lib/plugin.ts
+++ lib/plugin.ts
@@ -5,7 +5,18 @@ import type { Stripe } from '@stripe/stripe-js'
 let stripe: Stripe | null
 
 export async function getStripeInstance(): Promise<Stripe | null> {
-  if (!stripe) stripe = await loadStripe('<%= options.publishableKey %>')
+  let locale
+
+  const i18n = "<%= options.i18n %>"
+
+  if (i18n === "true") {
+    locale = window.$nuxt.$i18n.locale
+  }
+
+  if (!stripe)
+    stripe = await loadStripe("<%= options.publishableKey %>", {
+      locale: locale,
+    })
 
   return stripe
 }
