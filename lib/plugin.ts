7d7431789e0c36491ed7f63da0f8c5e51a388aa5
Fábio Carvalho <fabio.mac.95@gmail.com>
2021-01-28 15:49:29 +0000

    fix: stripe failed to load

--- lib/plugin.ts
+++ lib/plugin.ts
@@ -8,15 +8,29 @@ function _isTrue(val: string): boolean {
   return val === "true";
 }
 
+function retryDelay(retryCount: number): number {
+  const delay = 2 ** retryCount * 500;
+  return delay + delay * 0.2 * Math.random();
+}
+
 export async function getStripeInstance(locale?: StripeElementLocale | CheckoutLocale): Promise<Stripe | null> {
   if (!stripe) {
     if (!locale && _isTrue("<%= options.i18n %>")) {
       locale = window.$nuxt.$i18n.locale as StripeElementLocale | CheckoutLocale
     }
 
-    stripe = await loadStripe("<%= options.publishableKey %>", {
-      locale,
-    })
+    let retries: number = 0;
+
+    while (stripe === null && retries < 3) {
+      setTimeout(async () => {
+        stripe = await loadStripe("<%= options.publishableKey %>", { locale });
+        retries++;
+      }, retryDelay(retries));
+    }
+
+    if (stripe === null && retries === 3) {
+      throw new Error("nuxt-stripejs: error loading Stripe");
+    }
   }
 
   return stripe
